2b5b31c6a1b03aa30657cd20b8b4089c7542d4a5
lais <lais.paiva@atos.net>
2023-07-04 15:53:20 -0300

    teste

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "lais-vpc-ref" { # lais-vpc-ref é o nome para referenciar a VPC em outro código
+resource "google_compute_network" "lais-vpc-ref1" { # lais-vpc-ref é o nome para referenciar a VPC em outro código
 	name = "lais-vpc-df1" # df = desafio final
 	provider = google
 	auto_create_subnetworks = false
