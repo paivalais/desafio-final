b23a668c6d7becc9166ed015fcdbbfe0c4a64227
lais <lais.paiva@atos.net>
2023-07-04 14:41:43 -0300

    Adicionando pasta infra

---
+++ infra/vpc.tf
@@ -0,0 +1,5 @@
+resource "google_compute_network" "lais-vpc-ref" { # lais-vpc-ref é o nome para referenciar a VPC em outro código
+	name = "lais-vpc-df" # df = desafio final
+	provider = google
+	auto_create_subnetworks = false
+}
