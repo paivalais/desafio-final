13810fe86ccaafe245e867ad8fa3e25455b554aa
lais <lais.paiva@atos.net>
2023-07-07 16:25:08 -0300

    Adicionando scripts

--- aplicacao/app-info.sh
+++ aplicacao/app-info.sh
@@ -7,3 +7,11 @@ echo "Password: $(kubectl get secret my-grafana-admin --namespace deploy-ingress
 echo "============LOGIN INGO - WORDPRESS============"
 echo Username: user
 echo Password: $(kubectl get secret --namespace deploy-ingress my-wordpress -o jsonpath="{.data.wordpress-password}" | base64 -d)
+
+echo "============IP DO SERVIÇO DO PROMETHEUS============"
+namespace="deploy-ingress"
+service="my-prometheus-kube-prometh-prometheus"
+
+ip=$(kubectl get svc --namespace $namespace $service -o jsonpath="{.spec.clusterIP}")
+
+echo "IP do serviço $service na namespace $namespace: $ip"
