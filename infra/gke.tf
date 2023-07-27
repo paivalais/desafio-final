7e52841b37344caf22f36048d3bc281354be38d6
lais <lais.paiva@atos.net>
2023-07-27 17:27:12 -0300

    Adicionando campos faltantes no módulo.

--- infra/gke.tf
+++ infra/gke.tf
@@ -1,7 +1,7 @@
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-private-cluster-update-variant"
   project_id                 = "ces-igniteprogram"
-  name                       = "lais-clustergke-df-teste"
+  name                       = "lais-clustergke-df"
   region                     = "us-east1"
   zones                      = ["us-east1-b", "us-east1-c", "us-east1-d"]
   network                    = "lais-vpc-df"
@@ -12,12 +12,12 @@ module "gke" {
   network_policy             = false
   horizontal_pod_autoscaling = true
   filestore_csi_driver       = true
-  enable_private_endpoint    = false
+  #enable_private_endpoint    = false
   enable_private_nodes       = true
   master_ipv4_cidr_block     = "10.0.1.0/28"
-  istio                      = false
-  cloudrun                   = false
-  dns_cache                  = false
+  #istio                      = false
+  #cloudrun                   = false
+  #dns_cache                  = false
   remove_default_node_pool   = true
   depends_on = [google_compute_router_nat.nat]
 
@@ -25,17 +25,20 @@ module "gke" {
     {
       name                      = "lais-node-pool-df"
       machine_type              = "n1-standard-2"
-      node_locations            = "us-east1-b,us-east1-c"
+      node_locations            = "us-east1-b,us-east1-c,us-east1-d" ###
       initial_node_count        = 1
       max_count                 = 4
+      max_surge                 = 2 ##
+      max_unavailable           = 1 ##
       local_ssd_count           = 0
       spot                      = false
-      local_ssd_ephemeral_count = 0
+      # local_ssd_ephemeral_count = 0
       disk_size_gb              = 30
       disk_type                 = "pd-standard"
       image_type                = "COS_CONTAINERD"
-      enable_gcfs               = false
-      enable_gvnic              = false
+      #enable_gcfs               = false
+      #enable_gvnic              = false
+      autoscaling               = true ###
       auto_repair               = true
       auto_upgrade              = true
       service_account           = "sa-ignite-terraform-lais@ces-igniteprogram.iam.gserviceaccount.com"
